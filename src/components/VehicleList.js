2d084415a6e2f470808ccb34759e5e83be938031
MPOBLETE <maur.poblete@duocuc.cl>
2024-04-14 13:38:59 -0400

    Initial commit

---
+++ src/components/VehicleList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VehicleList = () => {
+  // Aquí implementaremos la lógica para mostrar la lista de vehículos
+  return (
+    <div className="row">
+        <div className="col-12 col-md-6 col-lg-4 mb-4">
+            {/* Aquí mostraremos la información de un vehículo */}
+        </div>
+        {/* Agrega más columnas para los demás vehículos */}
+    </div>    
+  );
+};
+
+export default VehicleList;
